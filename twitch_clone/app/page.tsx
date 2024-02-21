37dec0a6ae4b53784288d24fdf20dfba3cfa1343
Namprire <54724383+Namprire@users.noreply.github.com>
2024-02-21 19:51:19 +0100

    Add .env to .gitignore and create sign-in and sign-up pages

--- twitch_clone/app/page.tsx
+++ twitch_clone/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
+import {Button} from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <p>Only authenticated users can see this</p>
   );
 }
